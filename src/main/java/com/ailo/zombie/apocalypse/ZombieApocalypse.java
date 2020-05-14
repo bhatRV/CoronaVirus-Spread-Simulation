f9cd215873252375bfc7c1bbd424b93c0e0b917c
root <31539681+bhatRV@users.noreply.github.com>
2020-05-14 14:55:06 +1000

    added no input handling

--- src/main/java/com/ailo/zombie/apocalypse/ZombieApocalypse.java
+++ src/main/java/com/ailo/zombie/apocalypse/ZombieApocalypse.java
@@ -10,6 +10,15 @@ import java.util.List;
 public class ZombieApocalypse {
 
     public static void main(String[] args) throws SimulationException, InterruptedException {
+        if(args.length == 0)
+        {
+            System.out.println("Provide a valid fileName with all the inputs: \ninput file format\n "
+                + "<Line-1: Grid Dimension( eg, 4)> \n "
+                + "<Line 2: Zombie Location (eg. x,y) >\n "
+                + "<Line 3: creatures Locations (eg. x1,y1 x2,y2 x3,y3) >\n "
+                + "<Line4: Directions for the Zombie eg.DLLURL (where D: Down, R: Right L:Left and U: UP)>    ");
+            System.exit(0);
+        }
 
         List<String> lines = new ReadInputs().apply(args[0]);
         ZombieGrid[][] matrixZombieGrid = new MatrixGenerator().apply(lines);
